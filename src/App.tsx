00f637b4f90089ef62af870267e3e0bb834bbebb
IshDeshpa <ishdeshpa@gmail.com>
2024-08-19 01:27:34 -0500

    still under development...

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+
+import Menubar from './components/Menubar'
+
 import './App.css'
+import './scss/main.scss'
 
 function App() {
   const [count, setCount] = useState(0)
+  const navItems = ['Home', 'About', 'Services', 'Contact']
 
   return (
     <>
-      <div>
+      <Menubar name="My Website" logo={reactLogo} navItems={navItems} />
+      <div> 
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
