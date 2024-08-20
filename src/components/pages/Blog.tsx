7770dd54ad15a2359ad2a4141db351f52a3820e6
IshDeshpa <ishdeshpa@gmail.com>
2024-08-20 14:58:58 -0500

    fixed tslint error

--- src/components/pages/Blog.tsx
+++ src/components/pages/Blog.tsx
@@ -1,12 +1,11 @@
-import React, { useEffect, useState } from 'react';
 import Post from '../post/Post';
 
 const MyBlog: React.FC = () => {
     const posts = Object.values(import.meta.glob("./posts/**/*.md", {eager: true, query: "?raw"}));
 
     return (
-        <div className="ps-2">
-            {posts.map((post, index) => (
+        <div className="ps-2" id="Blog">
+            {posts.map((post: any, index) => (
                 <Post md={post.default as string} key={index}></Post>
             ))}
         </div>
