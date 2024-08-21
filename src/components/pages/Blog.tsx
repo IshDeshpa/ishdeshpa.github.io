1fce73b58a141dbf557d05d6618a0fd40b4a8032
IshDeshpa <ishdeshpa@gmail.com>
2024-08-21 17:57:27 -0500

    made navbar smaller and padding on blog better

--- src/components/pages/Blog.tsx
+++ src/components/pages/Blog.tsx
@@ -4,7 +4,7 @@ const MyBlog: React.FC = () => {
     const posts = Object.values(import.meta.glob("./posts/**/*.md", {eager: true, query: "?raw"}));
 
     return (
-        <div className="ps-2" id="Blog">
+        <div className="ps-2 pe-2" id="Blog" >
             {posts.map((post: any, index) => (
                 <Post md={post.default as string} key={index}></Post>
             ))}
