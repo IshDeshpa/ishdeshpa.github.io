6e1ab148c55bddec6148881e75f9550cd760968d
IshDeshpa <ishdeshpa@gmail.com>
2024-09-05 16:36:59 -0500

    Changed background color & added dev/prod resume display

--- src/components/pages/Resume.tsx
+++ src/components/pages/Resume.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import './resume.scss'
 
 const Resume: React.FC = () => {
+    const path = (import.meta.env.DEV)?'./dist/resume.pdf':'resume.pdf';
+
     return (
         <div className="m-2 p-2 resume" id="Resume">
             <h1 className="fw-bold text-decoration-underline">Resume</h1>
-            <embed src='resume.pdf' width="100%" height="800px" type="application/pdf" />
+            <embed src={path} width="100%" height="800px" type="application/pdf" />
         </div>
     );
 };
