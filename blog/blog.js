2d0d1ee638d9b00695fc3417c854607159e02c00
IshDeshpa <ishdeshpa@gmail.com>
2025-09-29 23:19:44 -0500

    cleaned up blog structure with ChatGPT (i'm not going to lie about it)

--- blog/blog.js
+++ blog/blog.js
@@ -1,20 +1,23 @@
 $(function () {
   $.getJSON("blog-list.json", function (posts) {
     posts.forEach(function (postPath) {
-      // Get file name without extension
-      const fileName = postPath.split('/').pop();             // e.g., "my-post.md"
-      const title = fileName.replace(/\.md$/, '');            // e.g., "my-post"
-      const htmlFile = title + ".html";                       // e.g., "my-post.html"
-      
-      console.log(htmlFile)
+      // Remove trailing slash if any
+      postPath = postPath.replace(/\/$/, '');
+
+      // Find the markdown filename (without extension) from the folder name
+      // Assuming folder contains one markdown file
+      $.get(postPath, function() {}, 'text')  // placeholder to avoid errors, real file name is derived below
+
+      const folderName = postPath.split('/').pop();  // e.g., "my-post"
+      const htmlFile = postPath + "/" + folderName + ".html"; // e.g., "my-post/my-post.html"
 
       // Create a list item with a link
       $('#posts').append(
-	$('<li>').append(
-	  $('<a>')
-	  .attr('href', htmlFile)
-	  .text(title.replace(/[-_]/g, ' ')) // Make it more readable
-	)
+        $('<li>').append(
+          $('<a>')
+            .attr('href', htmlFile)
+            .text(folderName.replace(/[-_]/g, ' ')) // Make it more readable
+        )
       );
     });
   });
